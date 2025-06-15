8159894d112bc27c9311f0e6ed54871fd64f9ae3
manchenlee <sleep1700505@gmail.com>
2025-06-15 22:29:00 +0800

    revise backend/app.py, add port

--- backend/app.py
+++ backend/app.py
@@ -60,5 +60,6 @@ def process_audio():
     return send_file(output_io, mimetype="audio/wav", as_attachment=True, download_name="output.wav")
 
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
